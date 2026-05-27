b5ee03921f336284987a170cf3b07173479a2b05
ruixu580-art <ruixu580@gmail.com>
2026-05-27 09:40:36 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.set_page_config(page_title="慧写歌", page_icon="🎵")
 # Logo 居中，宽度 180 像素
 col1, col2, col3 = st.columns([1, 2, 1])
 with col2:
-    st.image("logoPNG.PNG", width=300)
+    st.image("logoPNG.png", width=200)
 
 # ========== API Key 读取 ==========
 api_key = st.secrets.get("MUREKA_API_KEY")
